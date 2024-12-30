efe60e489aebcd63d12bfa68dfc479ca3e0814a1
Aayush Kumar Gupta <aayush@itobuz.com>
2024-12-30 18:44:22 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const inputTask = document.getElementById("task-input");
+const inputBtn = document.getElementById("task-btn");
+
+const arr = [];
+
+
+
+inputBtn.addEventListener("click",  function () {
+
+    const task = inputTask.value;
+    arr.push(task);
+    console.log(arr);
+    
+    const p = document.createElement('p');
+    const note = document.createTextNode(task);
+    p.appendChild(note);
+    document.querySelector('#tasks').appendChild(p);
+  }
+)
+
+
