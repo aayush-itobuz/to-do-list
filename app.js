0c6e82cc5ed0cc7932814f14773079db27aad624
Aayush Kumar Gupta <aayush@itobuz.com>
2024-12-30 18:45:53 +0530

    formatted

--- app.js
+++ app.js
@@ -3,19 +3,15 @@ const inputBtn = document.getElementById("task-btn");
 
 const arr = [];
 
+inputBtn.addEventListener("click", function () {
 
+  const task = inputTask.value;
+  arr.push(task);
+  console.log(arr);
 
-inputBtn.addEventListener("click",  function () {
-
-    const task = inputTask.value;
-    arr.push(task);
-    console.log(arr);
-    
-    const p = document.createElement('p');
-    const note = document.createTextNode(task);
-    p.appendChild(note);
-    document.querySelector('#tasks').appendChild(p);
-  }
+  const p = document.createElement('p');
+  const note = document.createTextNode(task);
+  p.appendChild(note);
+  document.querySelector('#tasks').appendChild(p);
+}
 )
-
-
