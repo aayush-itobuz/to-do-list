108715eb2cc7e5ed4d3da79e2568308b0dede83f
Aayush Kumar Gupta <aayush@itobuz.com>
2025-01-02 09:48:30 +0530

    2nd jan 2025

--- app.js
+++ app.js
@@ -1,17 +1,49 @@
+
+
 const inputTask = document.getElementById("task-input");
 const inputBtn = document.getElementById("task-btn");
+const taskList = document.querySelector('#tasks');
 
 const arr = [];
 
-inputBtn.addEventListener("click", function () {
+const addTask = () => {
+  const task = inputTask.value.trim();
+  if (task != '') {
+    const obj = {};
+    obj.title = task;
+    obj.completed = "false";
 
-  const task = inputTask.value;
-  arr.push(task);
-  console.log(arr);
+    arr.push(obj);
 
-  const p = document.createElement('p');
-  const note = document.createTextNode(task);
-  p.appendChild(note);
-  document.querySelector('#tasks').appendChild(p);
+    console.log(arr);
+  }
+  else {
+    alert("Please add a valid task!");
+  }
 }
-)
+
+const printTask = () => {
+  taskList.innerHTML = '';
+
+  for (let i = 0; i < arr.length; i++) {
+    const p = document.createElement('p');
+    const note = document.createTextNode(arr[i].title);
+
+    const div = document.createElement('div');
+    const btn = document.createElement('btn');
+    btn.classList.add("remove-btn");
+    btn.innerText = "X";
+    btn.style.color = "red";
+    p.appendChild(note);
+    taskList.appendChild(p);
+    taskList.appendChild(btn);
+
+    // p.appendChild(note);
+    // taskList.appendChild(p);
+  }
+}
+
+inputBtn.addEventListener("click", function () {
+  addTask();
+  printTask();
+});
